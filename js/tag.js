37a56fccb8fad409e5bb2f2b017d76aac241fbb7
Kaushik Thirthappa <thirthappa.kaushik@gmail.com>
2015-10-13 19:36:43 +0530

    more structure to the tags file

--- js/tag.js
+++ js/tag.js
@@ -1,38 +1,53 @@
 (function ($) {
 
-    var a = "<div class='simple-tags-block'> <input type='text' class='simple-tag-input' placeholder='add tags'> </div>";
 
     $.fn.tags = function (opts) {
         var $this = $(this);
+        var a = "<div class='simple-tags-block'> <input type='text' class='simple-tag-input' placeholder='add tags'> </div>";
         $this.css('display', 'none');
         $(a).insertAfter(this); // create our own div
-        $input = $this.siblings().find('.simple-tag-input')
-        $input.keydown( function (event) {
-            if (event.which == 13 || event.which == 9) {
-                event.preventDefault();
-                if ($(event.target).val() !== '') {
-                    if ($this.val().length > 0) {
-                        var val = $this.val()
-                          , values = val.split(',');
-
-                        values[values.length] = $(event.target).val();
-                        $this.val(values.join(','));
-                    } else {
-                        $this.val($(event.target).val());
-                    }
-                    createTag(event, $input);
-                }
-            }
-        });
+        $input = $this.siblings().find('.simple-tag-input'); // jQuery instance of the div
+        var tag = new Tag($this, $input, opts); // instantiate the instance
     };
 
-    function createTag (event, $input) {
+    var Tag = function ($el, $input, opts) {
+        this.$el = $el;
+        this.$input = $input;
+        this.opts = opts;
+
+        // bind events
+        this.$input.keydown(function (event) {
+            this.keyDown(event);
+        }.bind(this));
+    }
+
+    Tag.prototype.keyDown = function (event) {
+
+        // if the key pressed is Enter or Tab
+        if (event.which == 13 || event.which == 9) {
+            event.preventDefault();
+            if ($(event.target).val() !== '') {
+                if (this.$el.val().length > 0) {
+                    var val = this.$el.val()
+                      , values = val.split(',');
+
+                    values[values.length] = $(event.target).val();
+                    this.$el.val(values.join(','));
+                } else {
+                    this.$el.val($(event.target).val());
+                }
+                this.createTag(event);
+            }
+        }
+    };
+
+    Tag.prototype.createTag = function (event) {
 
         var $element = $(event.target)
           , el = "<span class='simple-tag'>" + $element.val() + "</span>";
 
-        $(el).insertBefore($input);
-        $input.val('');
-    }
+        $(el).insertBefore(this.$input);
+        this.$input.val('');
+    };
 
 })(jQuery);
